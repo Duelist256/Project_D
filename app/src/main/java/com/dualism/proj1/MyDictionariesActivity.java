f4d4835a50f4bc73b900dca11f4bcb28e424cedf
Duelist256 <selykov.iw@yandex.ru>
2017-03-13 23:46:00 +0300

    Lol kek

--- app/src/main/java/com/dualism/proj1/MyDictionariesActivity.java
+++ app/src/main/java/com/dualism/proj1/MyDictionariesActivity.java
@@ -2,12 +2,32 @@ package com.dualism.proj1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MyDictionariesActivity extends AppCompatActivity {
+
+    //String kekStr = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_dictionaries);
+    }
+
+    public void testButton(View view) {
+        TextView textView = (TextView) findViewById(R.id.editText);
+
+        /*if (textView.getText().toString().equals("Lol")){
+            kekStr = "Kek";
+            textView.setText(kekStr);
+        } else if (textView.getText().toString().equals("Kek")){
+            kekStr = "Chebureck";
+            textView.setText(kekStr);
+        } else {
+            kekStr = "Lol";
+            textView.setText(kekStr);
+        }*/
     }
 }
