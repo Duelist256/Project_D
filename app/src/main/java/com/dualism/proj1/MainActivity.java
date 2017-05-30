f941cb8542c1dea106529d0a8dcd4831fe41e9fb
Duelist256 <selykov.iw@yandex.ru>
2017-05-31 01:45:35 +0300

    Some changes

--- app/src/main/java/com/dualism/proj1/MainActivity.java
+++ app/src/main/java/com/dualism/proj1/MainActivity.java
@@ -1,9 +1,7 @@
 package com.dualism.proj1;
 
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
-import android.media.MediaPlayer;
+
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
@@ -13,34 +11,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.design.widget.NavigationView;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Base64;
-import android.util.Log;
+
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+
 import com.android.volley.toolbox.Volley;
 import com.dualism.proj1.DB.DatabaseHandler;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-import com.dualism.proj1.DB.WordTransl;
 import com.dualism.proj1.fragments.LearnWordsFragment;
-import com.dualism.proj1.fragments.Menu1;
-import com.dualism.proj1.fragments.Menu2;
+
 import com.dualism.proj1.fragments.Menu3;
 import com.dualism.proj1.fragments.MyDictionaryFragment;
 
@@ -113,7 +95,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Intent intent;
         switch (id) {
             case R.id.nav_my_dict:
-                //fragment = new Menu1();
                 fragment = new MyDictionaryFragment();
                 break;
             case R.id.nav_learn_words:
@@ -146,59 +127,4 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         return true;
     }
-
-    /*public void playAudio(String word) {
-        this.word = word;
-        String url = "http://54.218.48.30:8080/audio/"+this.word;
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        //mTextView.setText("Response is: "+ response);
-                        Log.d("Audio: ", "eee");
-                        //decoding received response string
-                        decodeAudio(response);
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                //mTextView.setText("That didn't work!");
-                Log.d("Audio: ", "That didn't work!");
-            }
-        }){
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String>  headers = new HashMap<String, String>();
-                headers.put("Content-Type", "application/json; charset=utf-8");
-                Intent intent = getIntent();
-                String base64Credentials = intent.getStringExtra("credentials");
-                headers.put("Authorization", "Basic "+base64Credentials);
-                return headers;
-            }
-        };
-    }
-
-    public void decodeAudio(String strAudio) {
-        byte[] audio = Base64.decode(strAudio, Base64.DEFAULT);
-        File outputFile = null;
-        try {
-            //write received byte array to file
-            outputFile = new File(getFilesDir()+File.separator+word+".flac");
-            FileOutputStream fileoutputstream = new FileOutputStream(outputFile);
-            fileoutputstream.write(audio);
-            fileoutputstream.close();
-            //done!
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            //testing audio file
-            mediaPlayer.setDataSource(outputFile.getAbsolutePath());
-            mediaPlayer.prepare();
-            mediaPlayer.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }*/
 }
